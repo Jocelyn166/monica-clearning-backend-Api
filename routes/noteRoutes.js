9459da08f0b5f916a7e6f1a6af51379d16eb76c0
Juan Wang <67444962+Jocelyn166@users.noreply.github.com>
2024-03-11 16:29:17 +1100

    code optimize

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const notesController = require('../controllers/notesController')
-const verifyJWT = require('../middleware/verifyJWT')
+const express = require("express");
+const router = express.Router();
+const notesController = require("../controllers/notesController");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.use(verifyJWT) // Apply verifyJWT middleware to all the routes inside this file
-router.route('/')
-      .get(notesController.getAllNotes)
-      .post(notesController.createNewNote)
-      .patch(notesController.updateNote)
-      .delete(notesController.deleteNote)
+router.use(verifyJWT); // Apply verifyJWT middleware to all the routes inside this file
+router
+  .route("/")
+  .get(notesController.getAllNotes)
+  .post(notesController.createNewNote)
+  .patch(notesController.updateNote)
+  .delete(notesController.deleteNote);
 
-module.exports = router
+module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const usersController = require('../controllers/usersController')
-const verifyJWT = require('../middleware/verifyJWT')
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controllers/usersController");
+const verifyJWT = require("../middleware/verifyJWT");
 
-router.use(verifyJWT) // Apply verifyJWT middleware to all the routes inside this file
+router.use(verifyJWT); // Apply verifyJWT middleware to all the routes inside this file
 
-router.route('/')
-      .get(usersController.getAllUsers)
-      .post(usersController.createNewUser)
-      .patch(usersController.updateUser)
-      .delete(usersController.deleteUser)
+router
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.createNewUser)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser);
 
-module.exports = router
+module.exports = router;
